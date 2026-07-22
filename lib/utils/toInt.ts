b1ec5502654672b7a1510300242602fd92955afa
LevyP07 <levypondman@live.nl>
2026-07-22 11:35:57 +0200

    change: constrain `toInt` constant fallback to an integer

--- lib/utils/toInt.ts
+++ lib/utils/toInt.ts
@@ -1,6 +1,18 @@
-// TODO: Write docs
+export type Integer<T extends number> = `${T}` extends `${string}.${string}` ? never : T;
 
-export function toInt(value: unknown, fallback: number = 0): number {
+/**
+ * Turns a value into an integer where invalid values, `NaN` or `Infinity` will return a fallback.
+ *
+ * The time complexity for this is `O(1)`.
+ *
+ * @template {number} T
+ * @param {?} value The value to convert
+ * @param {Integer<T>} [fallback = 0] An integer to fallback on when the value is invalid
+ * @returns {number} An integer number parsed from the value or a fallback
+ */
+export function toInt<const T extends number>(value: unknown, fallback: Integer<T> = 0 as Integer<T>): number {
+	const integerFallback = Math.trunc(fallback);
+
 	let result: number;
 
 	switch (typeof value) {
@@ -21,12 +33,12 @@ export function toInt(value: unknown, fallback: number = 0): number {
 			break;
 		}
 		default: {
-			return fallback;
+			return integerFallback;
 		}
 	}
 
 	if (Number.isNaN(result) || !Number.isFinite(result)) {
-		return fallback;
+		return integerFallback;
 	}
 
 	return result;
