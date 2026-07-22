3469d9ed27a55e0ca6d72b4fc5bd1bdfa1097036
LevyP07 <levypondman@live.nl>
2026-07-22 11:36:16 +0200

    chore(`toFloat`): docs

--- lib/utils/toFloat.ts
+++ lib/utils/toFloat.ts
@@ -1,5 +1,13 @@
-// TODO: Write docs
-
+/**
+ * Turns a value into an float where invalid values, `NaN` or `Infinity` will return a fallback.
+ *
+ * The time complexity for this is `O(1)`.
+ *
+ * @template {number} T
+ * @param {?} value The value to convert
+ * @param {Integer<T>} [fallback = 0] A float number to fallback on when the value is invalid
+ * @returns {number} A float number parsed from the value or a fallback
+ */
 export function toFloat(value: unknown, fallback: number = 0): number {
 	let result: number;
 
