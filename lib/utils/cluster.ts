81586b9f3ab74ebaae5577788f422b073a269051
ShadowWolf308 <levyvandervalk@gmail.com>
2026-07-28 15:07:09 +0200

    fix: cluster items correctly

--- lib/utils/cluster.ts
+++ lib/utils/cluster.ts
@@ -1,4 +1,5 @@
 import { klona } from "klona";
+import { UtilError } from "~/error";
 
 /**
  * Clusters a list of items into a list of lists of items limited to a specified size.
@@ -11,18 +12,26 @@ import { klona } from "klona";
  * @returns {T[][]} An array of arrays with a max size of the specified size
  */
 export function cluster<const T>(items: T[], size: number): T[][] {
+	if (size < 1) {
+		throw new UtilError("cluster", "Cluster size cannnot be smaller then 1");
+	}
+
+	if (!Array.isArray(items)) {
+		throw new UtilError("cluster", "Items must be an array");
+	}
+
 	const cloned = klona(items);
 	const clustered: T[][] = [[]];
 
 	let index = 0;
 
 	for (const item of cloned) {
-		if (clustered[index].length <= size) {
+		if (clustered[index].length < size) {
 			clustered[index].push(item);
 		} else {
 			index += 1;
 
-			clustered[index] = [];
+			clustered[index] = [item];
 		}
 	}
 
