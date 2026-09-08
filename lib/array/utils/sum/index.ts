2bde59430ff2493c909856bf3d463536f292e90c
LevyP07 <levypondman@live.nl>
2026-09-08 23:48:59 +0200

    change: convert non-numeric values in `sum` utility

--- lib/array/utils/sum/index.ts
+++ lib/array/utils/sum/index.ts
@@ -1,18 +1,20 @@
 /**
  * Sums a list of items into a total number.
  *
+ * If no mapper is specified, non-numeric values are converted to `1` if truthy and `0` if falsey.
+ *
  * The time complexity for this is `O(n)` where `n` is the amount of items in the list.
  *
  * @template T
- * @param {T[]} values The list of values to sum up
+ * @param {T[]} list The list of values to sum up
  * @param {(item: T) => number} mapper A callback to convert an item to be summable
  * @returns The sum of the list
  */
-export function sum<const T>(values: T[], mapper?: (item: T) => number): number {
+export function sum<const T>(list: T[], mapper?: (item: T) => number): number {
 	let result = 0;
 
 	if (mapper) {
-		for (const value of values) {
+		for (const value of list) {
 			const mapped = mapper(value);
 
 			if (Number.isNaN(mapped)) {
@@ -22,12 +24,14 @@ export function sum<const T>(values: T[], mapper?: (item: T) => number): number 
 			result += mapped;
 		}
 	} else {
-		for (const value of values) {
-			if (typeof value !== "number" || Number.isNaN(value)) {
+		for (const value of list) {
+			if (typeof value === "number" && !Number.isNaN(value)) {
+				result += value;
+
 				continue;
 			}
 
-			result += value;
+			result += value ? 1 : 0;
 		}
 	}
 
