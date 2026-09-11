7bdb3892305df2f0dd8b70f64f93f8171fe69c73
LevyP07 <levypondman@live.nl>
2026-09-11 17:03:55 +0200

    chore: `concat` documentation

--- lib/array/utils/concat/index.ts
+++ lib/array/utils/concat/index.ts
@@ -2,15 +2,16 @@ export type Concat<T extends (unknown[] | unknown)[]> = T extends [infer V, ...i
 	? [...(V extends readonly unknown[] ? V : [V]), ...Concat<R>]
 	: [];
 
-// TODO: Docs
-
 /**
+ * Returns an array based on the given values or values inside a given array.
+ *
  * The time complexity for this is `O(n)` where `n` is the amount of values.
  *
- * @param values
- * @returns
+ * @template {(? | ?[])[]} T
+ * @param {...T} values The values or array of values
+ * @returns {Concat<T>} A new array with all given values and values inside a given array
  */
-export function concat<const T extends (unknown[] | unknown)[]>(...values: T): Concat<T> {
+export function concat<const T extends (unknown | unknown[])[]>(...values: T): Concat<T> {
 	const base: unknown[] = [];
 
 	return base.concat(...values) as Concat<T>;
