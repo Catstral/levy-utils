49c093ad594c98f0cf3393783c8dc3157f0b2d75
LevyP07 <levypondman@live.nl>
2026-09-25 22:42:24 +0200

    change: rework `clamp` utility

--- lib/number/utils/clamp/index.ts
+++ lib/number/utils/clamp/index.ts
@@ -1,6 +1,31 @@
-export function clamp(value: number, start: number, end: number) {
-	const min = Math.min(start, end);
-	const max = Math.max(start, end);
+/**
+ * Clamps a number between a minimum and maximum value.
+ *
+ * Returns `NaN` if the minimum and maximum contradict each other.
+ *
+ * The time complexity for this is `O(1)`.
+ *
+ * @param {number} min The minimum value
+ * @param {number} value The value to be clamped between `min` and `max`
+ * @param {number} max The maximum value
+ * @returns {number} The clamped number
+ */
+export function clamp(min: number, value: number, max: number): number {
+	if (min > max) {
+		return NaN;
+	}
 
-	return Math.max(min, Math.min(value, max));
+	if (min <= value && value <= max) {
+		return value;
+	}
+
+	if (value < min) {
+		return min;
+	}
+
+	if (value > max) {
+		return max;
+	}
+
+	return NaN;
 }
